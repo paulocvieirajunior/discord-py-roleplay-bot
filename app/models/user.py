edcad599ee1bd6131f0a9cfecd6f656fb6cb5b38
Paulo Vieira <paulocvieirajunior@gmail.com>
2026-04-21 18:41:38 -0300

    ✨ Add user model

---
+++ app/models/user.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from sqlalchemy import BigInteger, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(BigInteger(), primary_key=True)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
